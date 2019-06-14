f3f0ab73da2f7f971cc0f79c7c59763747114fee
Alex <iserjude@yahoo.com>
2019-06-14 19:00:17 +0300

    Update my_software.py

--- my_software.py
+++ my_software.py
@@ -1,5 +1,7 @@
-click()
-welcome()
+import time
+
+print(time.asctime( time.localtime(time.time()) ))
+
 
 def click():
     print("Click")
@@ -11,5 +13,6 @@ def welcome():
     
 
 
-
+click()
+welcome()
 
